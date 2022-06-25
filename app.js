c766207077f5048565076b6adad817e96d2e4101
Lavish Goyal <goellavish10@gmail.com>
2022-06-25 17:43:33 +0530

    Procfile

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-require("dotenv").config("./config");
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+require('dotenv').config('./config');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
 const app = express();
 
 // Essential Middlewares
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan("tiny"));
+app.use(morgan('tiny'));
 
-app.use('/api/delegation', require('./routes/index'))
+app.use('/api/delegation', require('./routes/index'));
 
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening on PORT ${PORT}`);
 });
